0b164fba12cb06202dd37f05945fa57b446a254c
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-02 11:24:54 -0300

    List of items added without an interface to hide or show the content of each item.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,1 +1,20 @@
-import React from 'react';
+import React from "react";
+
+const Accordion = ({items}) => {
+  const renderedItems = items.map((item, index) => {
+    return (<React.Fragment key={item.title}>
+      <div className="title active" onClick={() => console.log("Item "+index+" clicked")}>
+        <i className="dropdown icon"></i>
+        {item.title}
+      </div>
+      <div className="content active">
+        <p>{item.content}</p>
+      </div>
+    </React.Fragment>)
+  });
+  return (<div className="ui styled accordion">
+    {renderedItems}
+  </div>);
+};
+
+export default Accordion;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import Accordion from "./Accordion";
 
-class App extends React.Component {
-  render(){
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creatin components'
+  },
+]
+
+export default () => {
+  return <Accordion items={items}/>;
 }
 
-export default App;
+

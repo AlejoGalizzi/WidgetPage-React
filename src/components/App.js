61d0eeaa469813af8f948c102d80ff68b3975b8c
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-03 18:36:08 -0300

    Functionality added to select one option and the rest stay hidden.

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
 import Dropdown from "./Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
+    title: "What is React?",
+    content: "React is a front end javascript framework",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers'
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
+    title: "How do you use React?",
+    content: "You use React by creating components",
   },
-]
+];
 
 const options = [
   {
-    label: 'The Color Red',
-    value: 'red'
+    label: "The Color Red",
+    value: "red",
   },
   {
-    label: 'The Color Green',
-    value: 'green'
+    label: "The Color Green",
+    value: "green",
   },
   {
-    label: 'A Shade of Blue',
-    value: 'blue'
-  }
-]
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 
 export default () => {
-  return (<div>
-    <Dropdown options={options}/>
-  </div>);
-}
+  const [selected, setSelected] = useState(options[0]);
 
-
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};

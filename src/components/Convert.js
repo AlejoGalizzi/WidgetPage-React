5ff21e64828158853fa70c235aa15f22a89d47e4
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-04 11:49:59 -0300

    Final version of translate's widget created.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import KEY from "../api/key";
 
-const Convert = ({language, text}) => {
-  
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text); 
+
   useEffect(() => {
-    console.log("New language and/or  text")
-  }, [language, text])
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
 
-  return <div></div>;
+      return () => {
+        clearTimeout(timerId);
+      }
+    }, 1000)
+  }, [text])
+ 
+  useEffect(() => {
+    const translate = async () => {
+      const {data} = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    translate();
+  }, [language, debouncedText]);
+
+  return <div><h1 className="ui header">{translated}</h1></div>;
 };
 
 export default Convert;
